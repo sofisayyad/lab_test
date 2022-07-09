9a4bff95be22d6add18d835d58ea42d1bda67cf9
sofisayyad <sofiyasayyad1999@gmail.com>
2022-07-09 21:46:55 +0530

    Changes done

---
+++ python.py
@@ -0,0 +1,11 @@
+#To make a simple calculator
+FN=int(input("Enter the first number="))
+SN=int(input("Enter the second number="))
+a=100+25
+b=100-25
+c=100*25
+d=100/25
+print("The addition of two numbers is=",a)
+print("The substraction of two numbers is=",b)
+print("The multiplication of two numbers is=",c)
+print("The division of two numbers is=",d)

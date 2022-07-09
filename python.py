04dc51e1e562456427dc9a14f9026811d478fc99
sofisayyad <107848124+sofisayyad@users.noreply.github.com>
2022-07-10 02:55:08 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,9 +1,2 @@
-#To make a simple calculator
-a=100+25
-b=100-25
-c=100*25
-d=100/25
-print("The addition of two numbers is=",a)
-print("The substraction of two numbers is=",b)
-print("The multiplication of two numbers is=",c)
-print("The division of two numbers is=",d)
+print("BTS")
+
